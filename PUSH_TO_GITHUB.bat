86f64db68c7c3b3b0b3a0945a518284425621ced
kachunchan-analytics <kachunchan.a@gmail.com>
2024-07-06 23:50:11 +0800

    New Updates for generating Equity Report

---
+++ PUSH_TO_GITHUB.bat
@@ -0,0 +1,7 @@
+@echo off
+cd /d "C:\Users\Andy\Desktop\python_work_github\Equity_Research_Tools\Equity_Research_Tools"
+rem cd /d %cd%
+python change_TXT_to_PY.py
+git add . 
+git commit -m "New Updates for generating Equity Report"
+git push
